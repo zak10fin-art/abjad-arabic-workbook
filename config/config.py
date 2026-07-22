2e4c4ef5a509587b358a6d8b6f7af57197bb3c02
Abjad Arabic Academy <zak10fin@gmail.com>
2026-07-22 15:42:47 +0100

    Add central configuration system

---
+++ config/config.py
@@ -0,0 +1,31 @@
+# Abjad Arabic Academy
+# Project Configuration
+
+BOOK = {
+    "title": "Arabic Alphabet Tracing & Coloring Workbook",
+    "author": "Abjad Arabic Academy",
+
+    # Page size
+    "width": 8.5,
+    "height": 11,
+
+    # Margins (inches)
+    "margin_top": 0.5,
+    "margin_bottom": 0.5,
+    "margin_left": 0.5,
+    "margin_right": 0.5,
+
+    # Fonts
+    "arabic_font": "Amiri",
+    "english_font": "Arial",
+
+    # Font sizes
+    "title_size": 28,
+    "letter_size": 140,
+    "word_size": 22,
+
+    # Folders
+    "data_folder": "data",
+    "template_folder": "templates",
+    "output_folder": "output"
+}
